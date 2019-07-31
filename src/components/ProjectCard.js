34f5c82950ed7e08e8955d74bc6a8e0ed3a73dd2
github username <github email address>
2019-07-31 19:17:01 -0400

    fixed links on cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,30 +6,81 @@ import { Card, Image} from 'semantic-ui-react'
 class ProjectCard extends Component  {
 
   render(){
-    debugger
+
+
+    let project = this.props.project
+
+    let blogPosts = ""
+    if (project.blogPosts){
+      blogPosts = project.blogPosts.map(post => (
+          <div key={post.index}><a href={post.link}>{post.title}</a></div>
+        ))
+    }
+
+    let tools = ""
+    if (project.tools){
+      tools = project.tools.map(tool => (
+        <div key={tool.index}> {tool} </div>
+      ))
+    }
+
+
+
+    let videoUrl = ""
+    if (project.video){
+      videoUrl = <h3><a href={project.video}>Video</a></h3>
+    }
+
+    let githubLink = ""
+    if (project.github.mono){
+        githubLink = <h3><a href={project.github.mono}>Github</a></h3>
+    } else if (project.github.frontEnd && project.github.backEnd){
+        githubLink =
+        <>
+          <h3><a href={project.github.frontEnd}>Github Front End</a> ---
+          <a href={project.github.backEnd}>Github Back End</a></h3>
+        </>
+    }
+
+    let demo = ""
+    if (project.demo){
+        demo = <h3><a href="{project.demo}">Demo</a></h3>
+    }
+
+
+
+
+
+
+
+
     return (
       <Card>
-        <Image src={this.props.project.image} />
+        <Image src={project.image} />
 
         <Card.Content >
           <Card.Header>
-            <h2>{this.props.project.name}</h2>
+            <h2>{project.name}</h2>
           </Card.Header>
 
           <Card.Description>
-            Project type: solo / group etc. <br/>
-            Notes go here <br/>
-            Blogposts here <br/>
-            embedded video?
+            <h4>{project.type} project</h4>
+            {project.contributors}
+            <br/>
+            <br/>
+            <div className = "left">
+              {project.notes}<br/>
+            </div>
+            {demo}
+            {videoUrl}
+            {githubLink}
+            <h3>Blog Posts </h3>
+            {blogPosts}
           </Card.Description>
         </Card.Content>
 
         <Card.Content extra >
-          <div>
-          Tools list
-          icons for ruby, javascript, etc.
-
-          </div>
+          {tools}
         </Card.Content>
       </Card>
     )
--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -26,7 +26,7 @@ class Projects extends Component {
         },
       video: "https://www.loom.com/share/7f2536bd3ee84514a88d96a6d242d46e",
       demo: "",
-      notes: "A guessing game about Who's Who in Congress",
+      notes: "A guessing game. Search and sort Congress members by party, gender and fun traits like most missed votes! Then guess who's who and click to reveal the answer. You can also get each Member's most recent bill and search all active bills by subject.",
       blogPosts: [
         {
           title: "Hey, Mr. Postman",
@@ -51,14 +51,16 @@ class Projects extends Component {
       tools: [
         "Ruby on Rails",
         "Javascript",
-        "Bootstrap"
+        "Bootstrap",
+        "OmniAuth",
+        "Devise"
       ],
       github: {
           mono: "https://github.com/joanrig/vacation_bidder"
         },
       video: "https://www.loom.com/share/b46f1f2896d64e1d9f90be2968037326",
       demo: "",
-      notes: "Add to your bucket list and then turn bucket list items into vacations.",
+      notes: "Add to your bucket list and then turn bucket list items into vacations. Add attractions and schedules to vacations. This is where I learned to secure logins, third-party login and conditional logic to render background pictures based on object attributes.",
       blogPosts: [
         {
           title: "Nested Routes and Nested Forms",
@@ -70,7 +72,7 @@ class Projects extends Component {
     let hogIron = {
       id: 3,
       name: "Hog Iron",
-      image: "https://i.kinja-img.com/gawker-media/image/upload/s--JHpsIlda--/c_scale,f_auto,fl_progressive,q_80,w_800/zjailhncxg4yutmlu3up.jpg",
+      image: "http://www.superhero-therapy.com/wp-content/uploads/2019/06/Sorting-Hat-1.jpg",
       type: "group",
       contributors: ["Lizzie Chan", "Vinh Do", "Bryn Lastname"],
       github: {
@@ -82,10 +84,9 @@ class Projects extends Component {
       ],
       video: "",
       demo: "https://hog-iron.herokuapp.com/",
-      notes: "I contributed logic for the sorting act and full-screen images/ Boostrap styling for most pages",
+      notes: "A mock Harry Potter site where you can be sorted into house and sign up for classes. I contributed logic for the sorting act and full-screen images/ Boostrap styling for most pages. Made with Ruby on Rails and Bootstrap.",
       blogPosts: ""
     }
-
 
     let spaceMissions = {
       id: 4,
@@ -97,11 +98,11 @@ class Projects extends Component {
           mono: "https://github.com/joanrig/Hog-Iron"
         },
       tools: [
-        "Ruby",
+        "Ruby", "Nokogiri"
       ],
       video: "https://www.loom.com/share/4c54a64adebd4bf3afb0913da06649df",
       demo: "",
-      notes: "Here's a fun way to sort through the Jet Propulsion Laboratory's space missions",
+      notes: "A command line interface program that allows you to sort NASA's Jet Propulsion Laboratory's space  missions by date and planet. Built with Ruby and now available for download as a Ruby Gem.",
       blogPosts: [
         {
           title: "Creating my own Ruby Gem: a Tale of Fear and Git",
