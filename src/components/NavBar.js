d30f0a502b69fecec46af6fce9cada4093a17909
github username <joanrigdon@gmail.com>
2019-10-28 16:50:07 -0400

    removed posts from navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,6 @@ const NavBar = () => {
                   <Link to='/' className="big grey">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                   <Link to='/projects' className="big grey">Projects</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                   <Link to='/resume' className="big grey">Resume</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  <Link to='/blog' className="big grey">Posts</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                   <a href='https://medium.com/@joanrigdon' className="big grey">Blog</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                   <Link to='/about' className="big grey">About</Link>
                 </div>
