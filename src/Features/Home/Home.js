f27bc0d0ba1af69b6747ad17f11d98c277b21ac7
github username <joanrigdon@gmail.com>
2019-10-31 01:06:34 -0400

    added fade in text to home page

--- src/Features/Home/Home.js
+++ src/Features/Home/Home.js
@@ -6,14 +6,18 @@ import { Link, Redirect } from 'react-router-dom'
 
 class Home extends Component {
   state = {
+    visible: false,
     redirect: false
+
   }
 
   componentDidMount() {
+    this.setState({ visible: true })
     this.id = setTimeout(() => this.setState({ redirect: true }), 4000)
   }
 
   componentWillUnmount() {
+    this.setState({ visible: false })
     clearTimeout(this.id)
   }
 
@@ -24,13 +28,18 @@ class Home extends Component {
       return <Redirect to="./projects" />
     } else {
       return(
-        <Link to="./projects">
-        <br/>
-        <h2 className="grey center">I'm Joan. Let's build something fabulous together ...</h2>
+          <div >
+          <Link to="./projects">
+            <br/>
+            <div className={this.state.visible?'fadeIn':'fadeOut'} >
+              <h2 className="grey center">I'm Joan. Let's build something fabulous together ...</h2>
+            </div>
+            <br/>
 
-        <Link to='/projects'><FerrisPic /></Link>
-        <br/>
-      </Link>
+            <Link to='/projects'><FerrisPic /></Link>
+            <br/>
+          </Link>
+          </div>
     )
     }
 
